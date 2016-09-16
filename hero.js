0871106643160caa300f93ee66b32ded1df50da6
Nikolaos Maniatis <maniatis@museotechniki.com>
2016-09-16 11:21:30 +0100

    bot init

--- hero.js
+++ hero.js
@@ -6,5 +6,16 @@ var T = new Twit(config);
 var stream = T.stream('statuses/filter', { track: '#herohelpline' })
 
 stream.on('tweet', function (tweet) {
-  console.log(tweet)
+  console.log(tweet);
+  var caller = tweet.user.screen_name;
+  var tweetId = tweet.id_str;
+
+ T.post('statuses/update', {status: 'Please @'+caller+' '+'come back later', in_reply_to_status_id: tweetId}, function (error, response) {
+    if (response) {
+      console.log('Success! Hero is on its way.');
+    }
+    if (error) {
+      console.log('There was an error with Twitter:', error);
+    }
+  });
 })
