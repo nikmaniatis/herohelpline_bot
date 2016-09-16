ef376ec1952798fbcca2e8252a1d65fdab3bdf74
Nikolaos Maniatis <maniatis@museotechniki.com>
2016-09-16 11:02:20 +0100

    twitter stream #herohelpline

--- hero.js
+++ hero.js
@@ -2,3 +2,9 @@ var Twit = require('twit');
 var config = require('./conf');
 
 var T = new Twit(config);
+
+var stream = T.stream('statuses/filter', { track: '#herohelpline' })
+
+stream.on('tweet', function (tweet) {
+  console.log(tweet)
+})
